0b8925b8fce0084ffd184c7dc236a7128558216b
anil <anil@841ebbcb-3b0a-4f52-9ef7-dde8961106df>
2014-01-07 04:44:22 +0000

    # Initial commit
    git-svn-id: file:///home/anil/tmp/test-repo@1 841ebbcb-3b0a-4f52-9ef7-dde8961106df

---
+++ test.php
@@ -0,0 +1,50 @@
+<?php
+
+$output = system("tar -czf test.tar.gx link/*", $retval);
+echo date('D, d-M-Y H00') . "\n";
+$date = date('d-m-Y');
+$date = strtotime($date);
+$date = strtotime("-7 day", $date);
+echo date('d-m-Y', $date) ."\n";
+
+
+$data = system("lf", $retval);
+echo "$data\n";
+echo "$retval\n";
+
+$data['section']['title']['email'] = 'something';
+
+foreach($data as $section=>$section_data )
+        {
+		    print_r("$section\n");
+		    print_r("$section_data\n");
+	            foreach($section_data as $title=>$title_data)
+		                {
+				       print_r("$title\n");
+				       print_r("$title_data\n");
+				       foreach($title_data as $email=>$email_data)
+						                {
+								print_r("$email\n");
+								print_r("$email_data\n");
+							    }
+							    }
+							    }
+
+$value = "";
+
+$ids  = explode(',', $value);
+
+if(!empty($ids))
+{
+    echo"Hello \n";
+    var_dump($ids);
+}
+else
+{
+    echo "bye\n";
+}
+
+
+
+preg_match_all('/[\p{Latin}]+/u', 'АБВГД ENGLISH STRING', $matches);
+print_r($matches);
